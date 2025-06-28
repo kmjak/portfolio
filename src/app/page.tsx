be217908ce771c825b2dd7199a79550e547e1bfe
kmjak <tymkoron1221@icloud.com>
2025-06-28 13:17:31 +0900

    feat: トップページが完成した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Button, PageTitle, Text } from "@/components/ui";
+import { PageTitle, Text } from "@/components/ui";
+import Image from "next/image";
 import Link from "next/link";
 
 /**
@@ -8,9 +9,20 @@ import Link from "next/link";
  */
 export default function Home() {
   return (
-    <div className="h-fit">
-      <div className="w-full mx-auto px-4 mt-28 md:mt-40">
+    <div className="h-full">
+      <div className="w-full mx-auto px-4 mt-6 md:mt-12">
         <div className="text-center">
+          <div className="flex justify-center mb-3">
+            <Image
+              src="/face.png"
+              alt="Maoto Okumuraの顔写真"
+              width={300}
+              height={300}
+              className="w-48 h-48 md:w-60 md:h-60 object-cover rounded-full select-none"
+              draggable={false}
+              priority
+            />
+          </div>
           <div className="mb-4 md:mb-8 space-y-4">
             <PageTitle className="text-5xl sm:text-5xl md:text-7xl lg:text-7xl font-bold">
               Maoto Okumura
@@ -26,13 +38,19 @@ export default function Home() {
             </Text>
           </div>
 
-          <div className="flex flex-col sm:flex-row gap-4 justify-center">
-            <Button className="px-8 py-4 bg-blue-700 font-semibold rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-200">
-              <Link href="/projects">プロジェクトを見る</Link>
-            </Button>
-            <Button className="px-8 py-4 bg-gray-800 backdrop-blur-sm font-semibold rounded-lg border border-gray-700 shadow-sm hover:shadow-md hover:bg-gray-700 transition-all duration-200">
-              <Link href="/contact">お問い合わせ</Link>
-            </Button>
+          <div className="flex flex-col sm:flex-row gap-4 justify-center my-1">
+            <Link
+              href="/projects"
+              className="px-8 py-4 bg-blue-700 font-semibold rounded-lg shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              プロジェクトを見る
+            </Link>
+            <Link
+              href="/contact"
+              className="px-8 py-4 bg-gray-800 backdrop-blur-sm font-semibold rounded-lg border border-gray-700 shadow-sm hover:shadow-md hover:bg-gray-700 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+            >
+              お問い合わせ
+            </Link>
           </div>
         </div>
       </div>

116719e4d88adfe96219320a05f79e82e280f372
kmjak <tymkoron1221@icloud.com>
2025-06-28 13:28:22 +0900

    style: Headerのheightを全体的に高くした

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -10,7 +10,7 @@ import { NavLinks } from "@/components/navigation/link";
  */
 export default function Header(): JSX.Element {
   return (
-    <header className="w-full h-10 md:h-12 lg:h-14 bg-gray-950 border-b border-gray-800 flex items-center">
+    <header className="w-full h-16 md:h-18 lg:h-20 bg-gray-950 border-b border-gray-800 flex items-center">
       <div className="container mx-auto flex items-center justify-between px-2">
         <HeaderLogo />
         <NavLinks />

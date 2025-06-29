fab8654fb053c48f68a634dc1ab52f302ea9e8cd
kmjak <tymkoron1221@icloud.com>
2025-06-29 13:09:00 +0900

    refactor: レート400とかではなくて入茶や入緑と表記する

--- src/components/about/AchievementsSection.tsx
+++ src/components/about/AchievementsSection.tsx
@@ -8,9 +8,9 @@ export default function AchievementsSection(): JSX.Element {
       <Text className="font-semibold">2025年06月: HackU 出場</Text>
       <Text className="font-semibold">2025年06月: 株式会社ログラス 5Daysインターンシップ参加</Text>
       <Text className="font-semibold">2025年03月: Progateハッカソン特別賞受賞</Text>
-      <Text className="font-semibold">2025年03月: AtCoder Heuristic: レート800到達</Text>
-      <Text className="font-semibold">2025年01月: AtCoder Heuristic: レート400到達</Text>
-      <Text className="font-semibold">2024年08月: AtCoder Algorithm: レート400到達</Text>
+      <Text className="font-semibold">2025年03月: AtCoder Heuristic: 入緑</Text>
+      <Text className="font-semibold">2025年01月: AtCoder Heuristic: 入茶</Text>
+      <Text className="font-semibold">2024年08月: AtCoder Algorithm: 入茶</Text>
       <Text className="font-semibold">2024年06月: AtCoder Heuristic: 初参加</Text>
       <Text className="font-semibold">2024年06月: AtCoder Algorithm: 初参加</Text>
       <Text className="font-semibold">その他: Call of Duty Mobile 元上位0.05%</Text>

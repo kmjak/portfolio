069187eb923a9ce5c4c0734b4ddeb2c8a9769d22
kmjak <tymkoron1221@icloud.com>
2025-06-30 03:53:38 +0900

    feat: 実際の資格の取得情報を用意した

---
+++ src/config/about/certifications.ts
@@ -0,0 +1,10 @@
+import { Certification } from "@/types/about";
+
+export const certifications: Certification[] = [
+  { date: "2024年05月", name: "CompTIA Security+" },
+  { date: "2023年12月", name: "CompTIA A+" },
+  { date: "2023年11月", name: "CompTIA Network+" },
+  { date: "2023年09月", name: "基本情報技術者" },
+  { date: "2023年08月", name: "ITパスポート" },
+  { date: "2023年07月", name: "Azure AI Fundamentals" },
+];

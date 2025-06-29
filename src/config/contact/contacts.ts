0dd220929bf42e9f3808c82e986ee249e62658a2
kmjak <tymkoron1221@icloud.com>
2025-06-29 14:48:25 +0900

    feat: 連絡先情報を用意した

---
+++ src/config/contact/contacts.ts
@@ -0,0 +1,16 @@
+import { Contact } from "@/types/contact";
+
+export const contacts: Contact[] = [
+  {
+    name: "GitHub",
+    value: "github.com/kmjak",
+    href: "https://github.com/kmjak",
+    description: "コードやプロジェクトをご覧いただけます",
+  },
+  {
+    name: "AtCoder",
+    value: "atcoder.jp/users/ApKr",
+    href: "https://atcoder.jp/users/ApKr",
+    description: "AtCoderのプロフィールです",
+  },
+];

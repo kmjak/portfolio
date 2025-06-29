0023e5c59eaf243bcbf0e14a3837269b7751cc15
kmjak <tymkoron1221@icloud.com>
2025-06-29 13:36:43 +0900

    feat: ヘッダーを画面上部に固定した

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -10,7 +10,7 @@ import { NavLinks } from "@/components/navigation/link";
  */
 export default function Header(): JSX.Element {
   return (
-    <header className="w-full h-16 md:h-18 lg:h-20 bg-gray-950 border-b border-gray-800 flex items-center">
+    <header className="fixed top-0 left-0 w-screen h-16 md:h-18 lg:h-20 bg-gray-950 border-b border-gray-800 flex items-center z-10">
       <div className="w-full flex items-center justify-between px-4">
         <HeaderLogo />
         <NavLinks />

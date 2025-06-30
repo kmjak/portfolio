52899d16c5d19ee9143308ec3453eeb2f7150124
kmjak <tymkoron1221@icloud.com>
2025-07-01 00:09:33 +0900

    feat: blobから写真を取得するためのコードをnext.config.tsに追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wjee3r3hkzrcatmm.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
+module.exports = nextConfig;
 export default nextConfig;

aa668f6ac43b6eab1e09c9cf516c3e268c31be8c
kmjak <tymkoron1221@icloud.com>
2025-06-30 22:42:36 +0900

    docs: それぞれの値の意味をJSDocで明示的に書いた

--- src/types/project/Project.ts
+++ src/types/project/Project.ts
@@ -1,5 +1,14 @@
 /**
  * @description プロジェクトの型定義
+ *
+ * @param id - プロジェクトの一意な識別子
+ * @param title - プロジェクトのタイトル
+ * @param description - プロジェクトの説明
+ * @param technologies - 使用した技術一覧
+ * @param githubUrl - GitHub リポジトリの URL (オプション）
+ * @param liveUrl - プロジェクトのライブデモの URL (オプション)
+ * @param imageUrl - プロジェクトの画像の URL (オプション)
+ * @param featured - 注目プロジェクトかどうか
  */
 export interface Project {
   id: string;

d095134946fbdbf9f336a3599471c158d217a874
kmjak <tymkoron1221@icloud.com>
2025-06-30 22:33:18 +0900

    feat: projectの型定義をした

---
+++ src/types/project/Project.ts
@@ -0,0 +1,13 @@
+/**
+ * @description プロジェクトの型定義
+ */
+export interface Project {
+  id: string;
+  title: string;
+  description: string;
+  technologies: string[];
+  githubUrl?: string;
+  liveUrl?: string;
+  imageUrl?: string;
+  featured: boolean;
+}

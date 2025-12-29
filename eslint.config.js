ba05faec17ccbf8280b3d28a3b35133662bd0d84
kmjak <tymkoron1221@icloud.com>
2025-12-29 11:51:54 +0900

    chore: Astroの雛形作成
    - Astroの雛形作成
    - Linter / Formatterの設定
    - Tailwind CSSの設定
    - husky / GitHub Actionsの設定

---
+++ eslint.config.js
@@ -0,0 +1,104 @@
+import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import astro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/prefer-const": "error",
+    },
+  },
+  ...astro.configs["flat/recommended"],
+  {
+    files: ["**/*.astro"],
+    rules: {
+      "astro/no-conflict-set-directives": "error",
+      "astro/no-unused-define-vars-in-style": "error",
+    },
+  },
+  {
+    files: ["**/*.{astro,tsx,jsx}"],
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      ...jsxA11y.configs.recommended.rules,
+      "jsx-a11y/alt-text": "error",
+      "jsx-a11y/anchor-is-valid": "warn",
+    },
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        astro: "readonly",
+      },
+    },
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-unused-vars": "off", // unused-importsプラグインに委任
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-undef": "error",
+      "no-unreachable": "error",
+      "no-unused-expressions": "error",
+      eqeqeq: ["error", "always"],
+    },
+  },
+  {
+    ignores: ["dist/**/*", ".astro/**/*", "node_modules/**/*", "public/**/*"],
+  },
+];
